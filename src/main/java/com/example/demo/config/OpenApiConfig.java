bce05612eb94ef134b7ed9001cd65990a3f4f61c
bala-amypo <bala@amypo.in>
2025-12-19 06:22:05 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,10 +1,10 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -12,9 +12,11 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9005.vs.amypo.ai")
-                ));
-        }
+                .info(new Info()
+                        .title("Volunteer Skill Matcher API")
+                        .version("1.0")
+                        .description("Backend API for Volunteer Skill Matcher"))
+                .addServersItem(new Server()
+                        .url("https://a7fda69f279b-8405.pro604cr.amypo.ai/proxy/9001/"));
+    }
 }
