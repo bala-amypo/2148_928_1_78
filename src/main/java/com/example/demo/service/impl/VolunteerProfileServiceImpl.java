52ca88f62fec3cdabeb371f2e8e13c10d7b23faa
bala-amypo <bala@amypo.in>
2025-12-26 07:46:00 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/VolunteerProfileServiceImpl.java
@@ -0,0 +1,66 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.VolunteerProfile;
+import com.example.demo.repository.VolunteerProfileRepository;
+import com.example.demo.service.VolunteerProfileService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VolunteerProfileServiceImpl implements VolunteerProfileService {
+
+    private final VolunteerProfileRepository repository;
+
+    public VolunteerProfileServiceImpl(VolunteerProfileRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public VolunteerProfile createVolunteer(VolunteerProfile volunteer) {
+
+        if (repository.existsByVolunteerId(volunteer.getVolunteerId())) {
+            throw new BadRequestException("Volunteer ID already exists");
+        }
+
+        if (repository.existsByEmail(volunteer.getEmail())) {
+            throw new BadRequestException("Email already exists");
+        }
+
+        if (repository.existsByPhone(volunteer.getPhone())) {
+            throw new BadRequestException("Phone already exists");
+        }
+
+        return repository.save(volunteer);
+    }
+
+    @Override
+    public VolunteerProfile updateAvailability(Long id, String availabilityStatus) {
+        VolunteerProfile volunteer = repository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Volunteer not found with id: " + id));
+
+        volunteer.setAvailabilityStatus(availabilityStatus);
+        return repository.save(volunteer);
+    }
+
+    @Override
+    public VolunteerProfile getVolunteerById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Volunteer not found with id: " + id));
+    }
+
+    @Override
+    public List<VolunteerProfile> getAllVolunteers() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Optional<VolunteerProfile> findByVolunteerId(String volunteerId) {
+        return repository.findByVolunteerId(volunteerId);
+    }
+}
